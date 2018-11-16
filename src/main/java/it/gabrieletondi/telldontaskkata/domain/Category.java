77ae32a880176c9ca8922ea62a756ecdafef9003
Alvaro Garcia <derecerca@gmail.com>
2018-11-16 16:19:36 +0100

    Refactor: remove dead code - parallel change end

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
@@ -5,19 +5,11 @@ import java.math.BigDecimal;
 public class Category {
     private BigDecimal taxPercentage;
 
-    public Category() {
-
-    }
-
     public Category(BigDecimal taxPercentage) {
         this.taxPercentage = taxPercentage;
     }
 
     public BigDecimal getTaxPercentage() {
         return taxPercentage;
-    }
-
-    public void setTaxPercentage(BigDecimal taxPercentage) {
-        this.taxPercentage = taxPercentage;
     }
 }
