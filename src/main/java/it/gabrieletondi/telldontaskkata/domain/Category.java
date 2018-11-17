2f9779369aaba24e993481adb6a294b7195282f2
Alvaro Garcia <derecerca@gmail.com>
2018-11-17 20:41:39 +0100

    Make this object a value object

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/Category.java
@@ -1,6 +1,7 @@
 package it.gabrieletondi.telldontaskkata.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Category {
     private final BigDecimal taxPercentage;
@@ -11,5 +12,27 @@ public class Category {
 
     public BigDecimal getTaxPercentage() {
         return taxPercentage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(taxPercentage, category.taxPercentage);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(taxPercentage);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Category{");
+        sb.append("taxPercentage=").append(taxPercentage);
+        sb.append('}');
+        return sb.toString();
     }
 }
