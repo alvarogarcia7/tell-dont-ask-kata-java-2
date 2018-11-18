5a685b995b0b92117cb4d578f8a89b40e72a6a1b
Alvaro Garcia <derecerca@gmail.com>
2018-11-18 17:28:44 +0100

    Refactor: remove dead code

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -9,32 +9,16 @@ public class OrderItem {
     private BigDecimal taxedAmount;
     private BigDecimal tax;
 
-    public Product getProduct() {
-        return product;
-    }
-
     public void setProduct(Product product) {
         this.product = product;
-    }
-
-    public int getQuantity() {
-        return quantity;
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    public BigDecimal getTaxedAmount() {
-        return taxedAmount;
-    }
-
     public void setTaxedAmount(BigDecimal taxedAmount) {
         this.taxedAmount = taxedAmount;
-    }
-
-    public BigDecimal getTax() {
-        return tax;
     }
 
     public void setTax(BigDecimal tax) {
